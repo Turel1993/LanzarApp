35c72a60929f692cdea15eadb84e8f95c2b5fa58
Luis <Luis@LTEJEDA>
2022-07-08 11:34:17 -0500

    Agregar archivos de proyecto.

---
+++ LanzarApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LanzarApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ LanzarApp/Platforms/Android/AbrirApp.cs
@@ -0,0 +1,21 @@
+﻿using Android.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace LanzarApp.Platforms.Android
+namespace LanzarApp
+{
+    public partial class AbrirApp
+    {
+        public partial void AbreApp()
+        {
+            Intent intent = new Intent();
+            intent.SetFlags(ActivityFlags.NewTask);
+            intent.SetComponent(new ComponentName("com.example.averesta", "com.example.averesta.MainActivity"));
+            Android.App.Application.Context.StartActivity(intent);
+        }
+    }
+}
